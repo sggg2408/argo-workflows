c8f08ff0efb9335a0e585b3b4ec49394da4829ef
sggg2408 <59368631+sggg2408@users.noreply.github.com>
2025-05-11 22:50:48 +0530

    Update flow.py

--- flow.py
+++ flow.py
@@ -1,1 +1,17 @@
+# flow.py
+from metaflow import FlowSpec, step
+
+class HelloFlow(FlowSpec):
+
+    @step
+    def start(self):
+        print("Hello, world!")
+        self.next(self.end)
+
+    @step
+    def end(self):
+        print("Flow finished.")
+
+if __name__ == '__main__':
+    HelloFlow()
 
